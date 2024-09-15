28c4704a62839e7dc2838fe1f02257785be1f753
gilberto <gilbertomoral61@gmail.com>
2024-09-15 01:55:20 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+### <p align='center'> ![logoMental](https://github.com/fabioVitorio/cursos_ead/assets/109548564/c96b8592-8588-4bee-9a37-74672b06c555) </p>
+<br>
+<p align='center'> Distance learning course website created at the beginning of my apprenticeship <br></p>
+
+##
+### ⚙️ Installation
+```bash
+~
+```
+##
+#### Functionalities
+- 🛡️ Login <br>
+- 🎞️ Video lessons <br>
+- 📱 Responsive <br>
+ #### Technologies
+- 🟡 JavaScript <br>
+- 🟠 HTML <br>
+- 🔵 CSS <br>
+
+<div>
+  <img height="100em" src="https://github-readme-stats.vercel.app/api/pin/?username=fabioVitorio&repo=cursos_ead"/>
+  <a href="https://github.com/fabioVitorio">
+</div>
+  
+#  
+![ianes](https://user-images.githubusercontent.com/109548564/196008559-97eac7d3-7a71-438d-bf75-0c97264dc8d6.PNG)
+![ianes2](https://user-images.githubusercontent.com/109548564/196008560-53fdfb01-619b-4bdf-8ee2-4dc385fa2926.PNG)
+![ianes3](https://user-images.githubusercontent.com/109548564/196008562-e0a4e1c8-25a8-4c70-8875-83ca799aa6e0.PNG)
---
+++ js/script_curso.js
@@ -0,0 +1,314 @@
+//-----------------------------------------------------------------------------------------------------------------------------------------------------
+//---------------------------------FUNÇÃO PARA CHAMAR O VÍDEO AO CLICAR NA BARRA LATERAL E TROCAR A SUGESTÃO DE VÍDEO----------------------------------
+//-----------------------------------------------------------------------------------------------------------------------------------------------------
+function chamaVideo(aula){
+    var aula0 = "https://www.youtube.com/embed/XlwST3iK93k?si=Zs23Rvcka3KEC2hz";
+    var aula1 = "https://www.youtube.com/embed/f5OW2ESKClE?si=aoo6Rvle1z0di-U8";
+    var aula2 = "https://www.youtube.com/embed/vDVgWgDgIss?si=KBVA2yl0m8lb9L9p";
+    var aula3 = "https://www.youtube.com/embed/neUFxe8k698?si=dJm7aAr3rV8mruJA";
+    var aula4 = "https://www.youtube.com/embed/aWdY6JUHXbc?si=JMVm7-aRHYAAS_ym";
+    var aula5 = "https://www.youtube.com/embed/fad5LbVLH6I?si=3peuVeMjKhY0vNEF";
+    var aula6 = "https://www.youtube.com/embed/S7zlXPcQzWU?si=37OuyFuuz6AqSbhJ";
+    var aula7 = "https://www.youtube.com/embed/2761ZI9f96g?si=gGsGSHcVvNRzcboO";
+    var aula8 = "https://www.youtube.com/embed/a1up4_n770s?si=nL9l208VVfX2g7pY";
+    var aula9 = "https://www.youtube.com/embed/Ya0qg4YXJfE?si=8ADZ4_41G8UJ1--1";
+    var aula10 = "https://www.youtube.com/embed/h2mlaB-BAJw?si=W1ZWjYCqPp8EenAN";
+    var aula11 = "https://www.youtube.com/embed/wJDuYAoKR_4?si=22Qb448t_yxnMxMM";
+    var aula12 = "https://www.youtube.com/embed/EmPWy_XjXZA?si=N2qrwZyr1n6WaX-a";
+    var aula13 = "https://www.youtube.com/embed/nP7bi5JjYJs?si=qwIgeZdxv07q-geE";
+    var aula14 = "https://www.youtube.com/embed/RktXOpaWssc?si=YN0ZzWFIUXApFKVY";
+    var aula15 = "https://www.youtube.com/embed/A05XbCVGnCA?si=BxQBdoggRtD5WBKq";
+    var aula16 = "https://www.youtube.com/embed/CmY5IoNdDa4?si=SLNhAeARn8qKiEvQ";
+    var aula17 = "https://www.youtube.com/embed/fOSeFVtM9Yk?si=jo9CBvfwoDHG-D6x";
+    var aula18 = "https://www.youtube.com/embed/0zVrVCneR_w?si=bH7Pzkpg2UCRwyf1";
+    var aula19 = "https://www.youtube.com/embed/UqDAufKGkZI?si=Xx_QAeTkzsSfxdTj";
+    var aula20 = "https://www.youtube.com/embed/qrs0Vmeu290?si=THmm7ZSfRb_MTtRx";
+    var aula21 = "https://www.youtube.com/embed/G9QXE1fTCdE?si=RYPQwhnzlNmntDQR";
+    var aula21 = "https://www.youtube.com/embed/r6ufqKRFAAk?si=Rj4VpMY99EEx6Fmm";
+    var aula22 = "https://www.youtube.com/embed/_ei9VyzL4hg?si=AcpshgBL5QY1ap_a";
+    var aula23 = "https://www.youtube.com/embed/MViF9XsDoK0?si=dHEIYbAD7f5545-F";
+    var aula24 = "https://www.youtube.com/embed/9Q6OGfnFTH4?si=7S5CEgCpS1jz5c-y";
+    var aula25 = "https://www.youtube.com/embed/k53QK3i2300?si=LSL4cdk6EvpQL0Um";
+    var aula26 = "https://www.youtube.com/embed/KGLGnKj8wPA?si=hzBd4S4wr1wLvqQN";
+    var aula27 = "https://www.youtube.com/embed/C-OELIg4yWg?si=Kn_KelusCQ2SVhld";
+    
+
+   
+
+    if(aula == "aula0"){
+        document.getElementById("iframe").src = aula0;
+
+        document.getElementById("imagem1").src = au1;
+        document.getElementById("imagem2").src = au3;
+        document.getElementById("imagem3").src = au4;
+
+    }
+    else if(aula == "aula1"){
+        document.getElementById("iframe").src = aula1;
+
+        document.getElementById("imagem1").src = au1;
+        document.getElementById("imagem2").src = au3;
+        document.getElementById("imagem3").src = au4;
+
+    }
+    else if(aula == "aula2"){
+        document.getElementById("iframe").src = aula2;
+
+        document.getElementById("imagem1").src = au1;
+        document.getElementById("imagem2").src = au3;
+        document.getElementById("imagem3").src = au4;
+
+    }
+    else if(aula == "aula3"){
+        document.getElementById("iframe").src = aula3;
+
+        document.getElementById("imagem1").src = au1;
+        document.getElementById("imagem2").src = au2;
+        document.getElementById("imagem3").src = au4;
+
+    }
+    else if(aula == "aula4"){
+        document.getElementById("iframe").src = aula4;
+
+        document.getElementById("imagem1").src = au1;
+        document.getElementById("imagem2").src = au2;
+        document.getElementById("imagem3").src = au3;
+        
+        document.getElementById("prova").href = "index.html";
+
+    }
+    else if(aula == "aula5"){
+        document.getElementById("iframe").src = aula5;
+
+        document.getElementById("imagem1").src = au1;
+        document.getElementById("imagem2").src = au2;
+        document.getElementById("imagem3").src = au3;
+        
+        document.getElementById("prova").href = "index.html";
+
+    }
+    else if(aula == "aula6"){
+        document.getElementById("iframe").src = aula6;
+
+        document.getElementById("imagem1").src = au1;
+        document.getElementById("imagem2").src = au2;
+        document.getElementById("imagem3").src = au3;
+        
+        document.getElementById("prova").href = "index.html";
+
+    }
+    else if(aula == "aula7"){
+        document.getElementById("iframe").src = aula7;
+
+        document.getElementById("imagem1").src = au1;
+        document.getElementById("imagem2").src = au2;
+        document.getElementById("imagem3").src = au3;
+        
+        document.getElementById("prova").href = "index.html";
+
+    }
+    else if(aula == "aula8"){
+        document.getElementById("iframe").src = aula8;
+
+        document.getElementById("imagem1").src = au1;
+        document.getElementById("imagem2").src = au2;
+        document.getElementById("imagem3").src = au3;
+        
+        document.getElementById("prova").href = "index.html";
+
+    }
+    else if(aula == "aula9"){
+        document.getElementById("iframe").src = aula9;
+
+        document.getElementById("imagem1").src = au1;
+        document.getElementById("imagem2").src = au2;
+        document.getElementById("imagem3").src = au3;
+        
+        document.getElementById("prova").href = "index.html";
+
+    }
+    else if(aula == "aula10"){
+        document.getElementById("iframe").src = aula10;
+
+        document.getElementById("imagem1").src = au1;
+        document.getElementById("imagem2").src = au2;
+        document.getElementById("imagem3").src = au3;
+        
+        document.getElementById("prova").href = "index.html";
+
+    }
+    else if(aula == "aula11"){
+        document.getElementById("iframe").src = aula11;
+
+        document.getElementById("imagem1").src = au1;
+        document.getElementById("imagem2").src = au2;
+        document.getElementById("imagem3").src = au3;
+        
+        document.getElementById("prova").href = "index.html";
+
+    }
+    else if(aula == "aula12"){
+        document.getElementById("iframe").src = aula12;
+
+        document.getElementById("imagem1").src = au1;
+        document.getElementById("imagem2").src = au2;
+        document.getElementById("imagem3").src = au3;
+        
+        document.getElementById("prova").href = "index.html";
+
+    }
+    else if(aula == "aula13"){
+        document.getElementById("iframe").src = aula13;
+
+        document.getElementById("imagem1").src = au1;
+        document.getElementById("imagem2").src = au2;
+        document.getElementById("imagem3").src = au3;
+        
+        document.getElementById("prova").href = "index.html";
+
+    }
+    else if(aula == "aula14"){
+        document.getElementById("iframe").src = aula14;
+
+        document.getElementById("imagem1").src = au1;
+        document.getElementById("imagem2").src = au2;
+        document.getElementById("imagem3").src = au3;
+        
+        document.getElementById("prova").href = "index.html";
+
+    }
+    else if(aula == "aula15"){
+        document.getElementById("iframe").src = aula15;
+
+        document.getElementById("imagem1").src = au1;
+        document.getElementById("imagem2").src = au2;
+        document.getElementById("imagem3").src = au3;
+        
+        document.getElementById("prova").href = "index.html";
+
+    }
+    else if(aula == "aula16"){
+        document.getElementById("iframe").src = aula16;
+
+        document.getElementById("imagem1").src = au1;
+        document.getElementById("imagem2").src = au2;
+        document.getElementById("imagem3").src = au3;
+        
+        document.getElementById("prova").href = "index.html";
+
+    }
+
+ else if(aula == "aula17"){
+        document.getElementById("iframe").src = aula17;
+
+        document.getElementById("imagem1").src = au1;
+        document.getElementById("imagem2").src = au2;
+        document.getElementById("imagem3").src = au3;
+        
+        document.getElementById("prova").href = "index.html";
+
+    }
+    else if(aula == "aula18"){
+        document.getElementById("iframe").src = aula18;
+
+        document.getElementById("imagem1").src = au1;
+        document.getElementById("imagem2").src = au2;
+        document.getElementById("imagem3").src = au3;
+        
+        document.getElementById("prova").href = "index.html";
+
+    }
+    else if(aula == "aula19"){
+        document.getElementById("iframe").src = aula19;
+
+        document.getElementById("imagem1").src = au1;
+        document.getElementById("imagem2").src = au2;
+        document.getElementById("imagem3").src = au3;
+        
+        document.getElementById("prova").href = "index.html";
+
+    }
+    else if(aula == "aula20"){
+        document.getElementById("iframe").src = aula20;
+
+        document.getElementById("imagem1").src = au1;
+        document.getElementById("imagem2").src = au2;
+        document.getElementById("imagem3").src = au3;
+        
+        document.getElementById("prova").href = "index.html";
+
+    }
+    else if(aula == "aula21"){
+        document.getElementById("iframe").src = aula21;
+
+        document.getElementById("imagem1").src = au1;
+        document.getElementById("imagem2").src = au2;
+        document.getElementById("imagem3").src = au3;
+        
+        document.getElementById("prova").href = "index.html";
+
+    }
+    else if(aula == "aula22"){
+        document.getElementById("iframe").src = aula22;
+
+        document.getElementById("imagem1").src = au1;
+        document.getElementById("imagem2").src = au2;
+        document.getElementById("imagem3").src = au3;
+        
+        document.getElementById("prova").href = "index.html";
+
+    }
+    else if(aula == "aula23"){
+        document.getElementById("iframe").src = aula23;
+
+        document.getElementById("imagem1").src = au1;
+        document.getElementById("imagem2").src = au2;
+        document.getElementById("imagem3").src = au3;
+        
+        document.getElementById("prova").href = "index.html";
+
+    }
+    else if(aula == "aula24"){
+        document.getElementById("iframe").src = aula24;
+
+        document.getElementById("imagem1").src = au1;
+        document.getElementById("imagem2").src = au2;
+        document.getElementById("imagem3").src = au3;
+        
+        document.getElementById("prova").href = "index.html";
+
+    }
+    else if(aula == "aula25"){
+        document.getElementById("iframe").src = aula25;
+
+        document.getElementById("imagem1").src = au1;
+        document.getElementById("imagem2").src = au2;
+        document.getElementById("imagem3").src = au3;
+        
+        document.getElementById("prova").href = "index.html";
+
+    }
+    else if(aula == "aula26"){
+        document.getElementById("iframe").src = aula26;
+
+        document.getElementById("imagem1").src = au1;
+        document.getElementById("imagem2").src = au2;
+        document.getElementById("imagem3").src = au3;
+        
+        document.getElementById("prova").href = "index.html";
+
+    }
+    else if(aula == "aula27"){
+        document.getElementById("iframe").src = aula27;
+
+        document.getElementById("imagem1").src = au1;
+        document.getElementById("imagem2").src = au2;
+        document.getElementById("imagem3").src = au3;
+        
+        document.getElementById("prova").href = "index.html";
+
+    }
+}
+function desabilitar(x){
+    x.disabled = false;
+}
